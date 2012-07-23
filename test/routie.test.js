56fff71d644a12eebcc1c109c7935f3f6a186e27
Greg Allen <me@jga.me>
2012-07-23 15:43:02 -0700

    more docs, more tests

--- test/routie.test.js
+++ test/routie.test.js
@@ -17,28 +17,61 @@ suite('routie', function() {
 
   test('pass in object', function(done) {
     routie({
-      'test': function() {
+      'test2': function() {
         done();
       }
     });
-    window.location.hash = 'test';
+    window.location.hash = 'test2';
   });
 
   test('trigger hash', function() {
-    routie('test');
-    assert.equal(window.location.hash, '#test');
+    routie('test3');
+    assert.equal(window.location.hash, '#test3');
   });
 
   test('regex support', function(done) {
 
-    routie('test/:name', function(name) {
+    routie('test4/:name', function(name) {
       assert.equal(name, 'bob');
       done();
     });
 
-    routie('test/bob');
-
+    routie('test4/bob');
   });
 
+  test('optional param support', function(done) {
+
+    routie('test5/:name?', function(name) {
+      assert.equal(name, undefined);
+      done();
+    });
+
+    routie('test5/');
+  });
+
+  test('wildcard', function(done) {
+    routie('test7/*', function() {
+      done();
+    });
+    routie('test7/123/123asd');
+  });
+
+  test('catch all', function(done) {
+    routie('*', function() {
+      done();
+    });
+    routie('test6');
+  });
+
+  /*TODO
+  test('route object passed as this', function(done) {
+    routie('*', function() {
+      console.log(this);
+      assert.equal(this.route, 'test7');
+      done();
+    });
+    routie('test7');
+  });
+  */
 
 });
